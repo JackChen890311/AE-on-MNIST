dba203a5ad241008841f0ff213ca763cfb314f60
Jack <a0930522803@gmail.com>
2023-03-30 22:15:21 +0800

    fix: fix parameters

--- constant.py
+++ constant.py
@@ -9,15 +9,15 @@ class CONSTANT():
         self.pm = True
         self.milestones = [50,500,5000]
         self.gamma = 0.5
-        self.patience = 20
+        self.patience = 50
         self.device = torch.device('cuda:0')
 
-        self.data_path = 'data/testing'
+        self.data_path = 'data/training'
         self.data_path_test = 'data/testing'
 
         self.hidden_dims = [392, 196, 98]
         self.image_dim = 28
         self.in_size = 784
-        self.latent_size = 8
+        self.latent_size = 16
 
         
